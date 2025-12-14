4366a125d42bdded1b4588208d3f66b6d5e817e5
Paola Aucapiña <102838577+PaolaMaribel18@users.noreply.github.com>
2025-12-14 17:29:57 -0500

    chore: :fire: archivos no utilizados/pruebas locales

--- backend/model_download.py
+++
@@ -1,4 +0,0 @@
-from transformers import pipeline
-
-modelo = pipeline("sentiment-analysis", model="finiteautomata/beto-sentiment-analysis")
-print("Modelo descargado y listo.")
--- fineTuning/finetuning.md
+++
@@ -1,1 +0,0 @@
-Aquí va lo de Wil
